59e71919edce8de415e76180d3b1ebfd3b5c2216
Noah Evans <noah@nevans.me>
2021-01-10 16:20:04 -0500

    Add guix de manifest

---
+++ guix-manifests/de.scm
@@ -0,0 +1,63 @@
+;; -*- geiser-scheme-implementation: guile -*-
+(specifications->manifest
+ '("nss-certs"
+
+   ;; WM
+   "st-clone"
+   "dwm-clone"
+   "polybar-dwm"
+   "dmenu-clone"
+   "picom"
+   "wmctrl"
+
+   ;; Audio utils
+   "alsa-utils"
+   "pavucontrol"
+   "pulsemixer"
+   "mpd-mpc"
+   "mpdscribble"
+
+   ;; Shell utils
+   "openssh"
+   "curl"
+   "wget"
+   "tmux"
+   "git"
+   "xclip"
+   "feh"
+   "neofetch"
+   "strace"
+   "zip"
+   "unzip"
+   "lm-sensors"
+   "flameshot"
+   "sysstat"
+   "zip"
+   "scrot"
+   "ncurses"  ;; For clear command
+   
+   ;;
+   ;; Programs
+   ;;
+
+   ;; Tui
+   "nano"
+   "vim"
+   "neovim"
+   "weechat"
+   "rtorrent-ps"
+   "ncmpcpp"
+   "ranger"
+   "syncthing"
+
+   ;; Gui
+   "mpv"
+   ;;"mcg"
+   "calibre"
+   "keepassxc"
+   "zathura"
+   "zathura-pdf-mupdf"
+   "sxiv-clone"
+   "thunar"
+   "font-awesome"
+   "syncthing-gtk"))
